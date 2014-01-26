ee628fd3bdf2f7f46b504496843147ca632503d1
Kensuke Nagae <kyanny@gmail.com>
2014-01-26 21:50:40 +0900

    rails new APP_NAME

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CompareLinkerApp::Application.config.secret_key_base = '21858abd75e51195eb7236d2255b9f49f7c9f93ebc87adfe6f69a4c50b0b38df3f8f8b69cbe094925e0eb418dc0716045281d18b3fe02be6c7b127e7669c3b18'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CompareLinkerApp::Application.config.session_store :cookie_store, key: '_compare_linker_app_session'
